70feb1616270ec87488f4912a136dc34f4d33618
graemefoster <graemefoster@microsoft.com>
2022-05-03 13:46:21 +0800

    Support data disks on vms

--- AzureDiagrams/Resources/VM.cs
+++ AzureDiagrams/Resources/VM.cs
@@ -11,12 +11,20 @@ public class VM : AzureResource, IAssociateWithNic
     public override string Image => "img/lib/azure2/compute/Virtual_Machine.svg";
     public override string? Fill => "#D5E8D4";
     public string SystemDiskId { get; protected set; } = default!;
+
+    public string[] DataDiskIds { get; set; } = default!;
     public string BootDiagnosticsStorageHost { get; protected set; } = default!;
     public string[] Nics { get; protected set; } = default!;
 
     public override Task Enrich(JObject jObject, Dictionary<string, JObject?> additionalResources)
     {
         SystemDiskId = jObject["properties"]!["storageProfile"]!["osDisk"]!["managedDisk"]!.Value<string>("id")!;
+        DataDiskIds =
+            jObject["properties"]!["storageProfile"]!["dataDisks"]
+                ?.Select(x => x["managedDisk"]?.Value<string>("id"))
+                .Where(x => x != null)
+                .Select(x => x!)
+                .ToArray() ?? Array.Empty<string>();
 
         BootDiagnosticsStorageHost =
             jObject["properties"]!["diagnosticsProfile"]!["bootDiagnostics"]!.Value<string>("storageUri")!;
@@ -27,12 +35,21 @@ public class VM : AzureResource, IAssociateWithNic
         return Task.CompletedTask;
     }
 
+
     public override void BuildRelationships(IEnumerable<AzureResource> allResources)
     {
-        var disk = allResources.OfType<Disk>()
-            .Single(x => string.Equals(x.Id, SystemDiskId, StringComparison.InvariantCultureIgnoreCase));
+        var disk = allResources.OfType<Disk>().Single(x => string.Equals(x.Id, SystemDiskId, StringComparison.InvariantCultureIgnoreCase));
         CreateFlowTo(disk, Plane.Runtime);
         OwnsResource(disk);
+
+        DataDiskIds.Select(x =>
+                allResources.OfType<Disk>().Single(x => string.Equals(x.Id, SystemDiskId, StringComparison.InvariantCultureIgnoreCase))
+            )
+            .ForEach(dataDisk =>
+            {
+                CreateFlowTo(dataDisk, Plane.Runtime);
+                OwnsResource(dataDisk);
+            });
 
         var allNics = Nics.Select(nic =>
             allResources.OfType<Nic>().Single(x => x.Id.Equals(nic, StringComparison.InvariantCultureIgnoreCase)));
